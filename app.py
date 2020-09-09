520accf619ebf5926a26aeabd82c917cf5b1105a
Diego448 <diegosg448@gmail.com>
2020-09-09 17:52:31 -0500

    Updating assets and links

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def homepage():
     return render_template('homepage.html')
+
+@app.route('/snacks/add')
+def add_snacks():
+    return render_template('add_snacks.html')
