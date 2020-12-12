cd278683634e1058fc70f4794f2df3f55ed4c798
alandavaloss98 <alanro32@gmail.com>
2020-12-11 19:52:36 -0600

    Updating homepage

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ def snack_edit(id):
 
 @app.route('/sales/all')
 def sales():
-    return render_template('sales.html', content=current_lang['sales'])
+    return render_template('sales.html')
 
 @app.route('/sales/add')
 def add_sales():
